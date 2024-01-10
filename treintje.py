32649ce4233f6003946ed2abcb0d3b20e1382eb8
MetteJoelle <mette.van.splunteren@hotmail.com>
2024-01-10 13:28:18 +0100

    eerste commit treintje

---
+++ treintje.py
@@ -0,0 +1,12 @@
+class Verbinding():
+    def __init__(self, tijd, station1, station2):
+        self.tijd = tijd
+        self.station1 = station1 
+        self.station2 = station2
+
+class Station():
+    def __init__(self, name, x_cor, y_cor):
+        self.name = name
+        self.x_cor = x_cor 
+        self.y_cor = y_cor 
+    
