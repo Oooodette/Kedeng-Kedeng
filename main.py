a45119678322882801dc5f08da9d0cc9bf05e2da
BasTreur01 <bas.treur@student.uva.nl>
2024-01-12 12:15:16 +0100

    main changes

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+<<<<<<< HEAD
 from code.classes import stations, connection, trajectory, network
 # from code.algorithms import
 # from code.algorithms import
 from code.visualize import visualize as vis
+
+
 
 if __name__ == "__main__":
     map_name = "nl"
@@ -9,13 +12,13 @@ if __name__ == "__main__":
     # Create network from our data
     test_network = Network('data\ConnectiesHolland.csv', 'data\StationsHolland.csv')
 
-
     test_network.create_stations()
     test_network.create_connections()
-    test_network.create_network()
+=======
+    test_network = network.Network('data\ConnectiesHolland.csv', 'data\StationsHolland.csv')
+    test_network.load_stations()
+    test_network.load_connections()
 
-    # show score of this network
-    test_network.get_score()
 
     # visualize
     vis.visualize_network('data/StationsNationaal.csv', 'data/ConnectiesNationaal.csv', 'output.csv')
