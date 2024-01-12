2a66c51afb241a064df7c5ee0e749a68445890c4
Odette Bonnema <odette.bonnema@gmail.com>
2024-01-12 11:09:06 +0100

    organized code, data and docs

---
+++ Code/classes/trajectory.py
@@ -0,0 +1,5 @@
+class Trajectory():
+    def __init__(self, name, stations, time):
+        self.name = name
+        self.stations = stations
+        self.time = time
--- representations.py
+++ representations.py
@@ -1,4 +1,4 @@
-from tkinter import W
+    from tkinter import W
 import pandas as pd
 import random
 
@@ -15,7 +15,7 @@ class Connection():
         self.time = time
         self.station1 = station1
         self.station2 = station2
-        self.driven = False
+        self.used= False
 
 
 class Trajectory():
@@ -26,15 +26,18 @@ class Trajectory():
 
 
 
-class Dienstregeling():
-    def __init__(self, connections_df, stations_df):
-        self.connections_df = connections_df
-        self.stations_df = stations_df
-        self.connections = []
-        self.stations = []
+class Network():
+    def __init__(self, connections_file, stations_file):
+        self.connections_df = load_data(connections_file)
+        self.stations_df = load_data(stations_file)
+        self.connections = [] #TODO: call function here
+        self.stations = [] # TODO: call function here
         self.trajectories = []
+        self.quality_network = None
         
-    
+    def load_data(self, filepath): 
+        return pd.read_csv(filepath, index_col=False)
+        
     def create_connections(self):
 
         for index, connection in self.connections_df.iterrows():
@@ -112,23 +115,26 @@ class Dienstregeling():
                     current_station = new_connection.station1
                 time += new_connection.time 
                 trajectory_stations.append(current_station)
-                new_connection.driven = True
+                new_connection.used = True
                 previous_connection = new_connection 
          
 
             # if no valid connection is found, break the loop
             else:
                 break
-            
-     
         
         new_trajectory = Trajectory('x', trajectory_stations, time) 
         return new_trajectory
-
+    def is_valid(self):
+        if all ([connection.used==True for connection in self.connections]):
+            return True
+        else:
+            return False
+    
     def create_network(self):
         counter = 1
-        # check if all connections are used
-        while not all ([connection.driven==True for connection in self.connections]):
+        # check if all connections are used and keep making trajectories 
+        while not self.is_valid():
             
             new_trajectory = self.create_trajectory()
             new_trajectory.name = counter
@@ -138,7 +144,7 @@ class Dienstregeling():
         # calculate score for this network
         fraction = 1 #TODO: calculate total number of connections used
         total_time = sum([trajectory.time for trajectory in self.trajectories])
-        quality_network = fraction * 10000 - (len(self.trajectories) * 100 + total_time)
+        self.quality_network = fraction * 10000 - (len(self.trajectories) * 100 + total_time)
 
         # generate output
         print(self.trajectories)
@@ -148,11 +154,6 @@ class Dienstregeling():
 
         output_df.to_csv('output.csv', index=False)
 
+    def get_score(self):
+        print(f'the score of this network = {self.quality_network}')
 
-connections_df = pd.read_csv('ConnectiesHolland.csv', index_col=False)
-stations_df = pd.read_csv('StationsHolland.csv', index_col=False)
-nieuwe_regeling = Dienstregeling(connections_df, stations_df)
-nieuwe_regeling.create_stations() 
-nieuwe_regeling.create_connections()
-# print(len(nieuwe_regeling.stations))
-nieuwe_regeling.create_network()
