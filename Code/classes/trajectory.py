94ea2df7ecab9678f81dfde31474fea73291b6bb
MetteJoelle <mette.van.splunteren@hotmail.com>
2024-01-15 13:41:48 +0100

    description of three classes

--- Code/classes/trajectory.py
+++ Code/classes/trajectory.py
@@ -1,5 +1,15 @@
 class Trajectory():
+    """
+    Class that creates a trajectory of train connections 
+
+    Attributes:
+    - name (int): the number of the trajectory, the first, second etc. 
+    - stations (list): list of stations to check the connections each station has 
+    - time (int): the total time of the trajectory 
+    - route (list): list of the connections of the trajectory 
+    """
     def __init__(self, name, stations, time):
         self.name = name
         self.stations = stations
         self.time = time
+        self.route = []
