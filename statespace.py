03f3d4047b3c0b652a2fe99b46f4c51c40baea37
BasTreur01 <bas.treur@student.uva.nl>
2024-01-16 10:02:59 +0100

    statespace changes

--- statespace.py
+++ statespace.py
@@ -3,8 +3,8 @@ def calc_statespace(max_traject_time, max_traject_num):
     """calculate statespace with given assumptions"""
 
     statespace = 0
-    for i in range(max_traject_time):
-        
+    for i in range(5, max_traject_time):
+
         nr_connections_trajectory = (i/5)
         n = 3
         r = nr_connections_trajectory
@@ -14,7 +14,8 @@ def calc_statespace(max_traject_time, max_traject_num):
 
         for j in range(max_traject_num):
             num_combinations = j * nr_of_trajectories_total
-            statespace += num_combinations
+            if (num_combinations  %  1) == 0:
+                statespace += num_combinations
 
     print(statespace)
 
