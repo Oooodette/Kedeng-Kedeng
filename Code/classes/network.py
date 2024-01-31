54f075446e2c6e0385ace79c162a2d62a82bc2a4
BasTreur01 <bas.treur@student.uva.nl>
2024-01-31 18:22:39 +0100

    network

--- Code/classes/network.py
+++ Code/classes/network.py
@@ -5,38 +5,51 @@ import pandas as pd
 import random
 class Network():
     """
-    Class Network: create Network object;
-        Attributes:
-        - connections (list of connection objects)
-        - stations (list of station objects)
-        - trajectories (list of trajectory objects)
-        - quality network (float) - quality specified as K = p * 10000 - (T * 100 + min); where:
-          p = fraction of driven connections; T = number of trajectories; min = total used time of the network
-        - used (dict) - dict that records whether connection has been used or not; in format connection: boolean
+    Class Network: create a network.
 
+        Attributes:
+        - connections (list): list of connection objects
+        - stations (list): list of station objects
+        - max_trajectories (int): max number of trajectories allowed in the network
+        - max_trajectories (int): max length a trajectory can be
+        - trajectories (list): list of trajectory objects
+        - quality network (float): quality specified as K = p * 10000 - (T * 100 + min); where:
+            p = fraction of driven connections; T = number of trajectories; min = total used time of the network
+        - used (dict): dict that records whether connection has been used or not; (key=connection, value=int)
+        - available_connections (dict): dict with connections of stations; (key=station.name, value=list of connections)
+        
         Methods:
-        - load_data - read in csv-files
-        - load_connections - create connection objects
-        - load_stations - create station objects
-        - add_trajectory - add trajectory to the network
-        - #remove_trajectory#
-        - #change_trajectory#
-        - get_score - retrieves the score of the network
+        - create_available_connections(): create the available connections dictionary
+        - load_connections(): create connection objects
+        - load_stations(): create station objects
+        - connections_used(): create the used dictionary
+        - add_trajectory(): add a trajectory to the network
+        - remove_trajectory(): remove a trajectory from the network
+        - is_valid(): checks if all connections are used
+        - calculate_score(): calculates score of the network
+        - save(): saves the networks trajectories and score to a csv
+        - get_score ()- retrieves the score of the network
     """
     def __init__(self, connections, stations, max_trajectories, max_trajectory_time):
         """
-        Method that initializes attributes of the network object
+        Initializes network and its attributes
+
         Args:
-        - connections_file: the file containing the train connections
-        - stations file: the file containing the train stations
+        - connections (pd.DataFrame or list): the dataframe or list containing the connections
+        - stations (pd.DataFrame or list): the dataframe or list containing the stations
+        - max_trajectories (int): max number of trajectories allowed in the network
+        - max_trajectory_time (int): max time a trajectory is allowed to be
         """
+
+        # check if connections (and stations) are a list or dataframe
         if type(connections) == list:
             self.connections = connections
             self.stations = stations
         else:
             self.connections = self.load_connections(connections)
             self.stations = self.load_stations(stations)
-            
+        
+        # assign attributes
         self.max_trajectories = max_trajectories
         self.max_trajectory_time = max_trajectory_time
         self.trajectories = []
@@ -45,23 +58,20 @@ class Network():
         self.available_connections = self.create_available_connections()
 
     def create_available_connections(self): 
-        """Creates a list of all connections that have the current station as one of their stations
-
-        Args: 
-        - current_station(str): name of current station
-        - connection_list(list): list of instances of connection class available for this network
+        """Creates a list of all connections that have the current station as one of their stations.
 
         Returns: 
-        - all_connections(list): list of instances of connection class that have current station as one of their stations.
+        - available_connections (dict): dict that has all available connections to a station; (key=station.name, value=list of connection objects)
         """
+
         station_list = self.stations
         connection_list = self.connections
         available_connections = {}
         for station in station_list:
             all_connections = [] 
+
             # loop through your list of connections and look for a connection that has the current station as station 1 or 2
             for connection in connection_list:
-
                 if connection.station1 == station.name or connection.station2 == station.name:
 
                     # create list of all stations that have current station as station 1
@@ -76,11 +86,12 @@ class Network():
     
     def load_connections(self, connections_df):
         """
-        Method to create a list of connection instances 
+        Create a list of connection instances from a dataframe containing the connections.
+
         Args:
-        - connections_df: the dataframe containing the connections 
+        - connections_df (pd.DataFrame): the dataframe containing the connections 
         Returns:
-        - a list containing the connection instances 
+        - connections (list): list containing the connection instances 
         """
         connections = []
 
@@ -97,12 +108,12 @@ class Network():
 
     def load_stations(self, stations_df):
         """
-        Method that creates a list of stations instances from a dataframe 
-        containing the stations 
+        Create a list of stations instances from a dataframe containing the stations.
+
         Args: 
-        - station_df: the dataframe containing the stations 
+        - station_df (pd.DataFrame): the dataframe containing the stations 
         Returns:
-        - a list containing the stations instances 
+        - stations (list): list containing the stations instances 
         """
         stations = []
 
@@ -119,41 +130,46 @@ class Network():
     
     def connections_used(self):
         """
-        Method that creates a dictionary named 'used' with the connection instances as attribute 
-        and whether the connection is already used or not as value with True and False 
-        To start, the connections values are all set to False because no connections are used yet 
+        Create a dict that records how many times a connection is used.
+
         Returns:
-        - the completed dictionary 
+        - used (dict): dict that for every connection records how many times it is used (key=connection object, value=int)
         """
+
         used = {}
+
+        #loop over all connections and set default to 0 (unused)
         for connection in self.connections:
             used[connection] = 0
+
         return used
  
-
     def add_trajectory(self, trajectory):
         """
-        Method that adds a trajectory to the trajectory list 
+        Adds a trajectory to the trajectory list 
+
         Args:
-        - trajectory: an instance of the trajectory class 
+        - trajectory (trajectory object): trajectory to append to the network
         """
         self.trajectories.append(trajectory)
 
     def remove_trajectory(self, trajectory):
         """
-        Method that removes a trajectory from the trajectory list 
+        Remove a trajectory from the trajectory list 
+
         Args:
-        - trajectory: an instance of the trajectory class 
+        - trajectory (trajectory object): trajectory to remove from the network
         """
         self.trajectories.remove(trajectory)
 
     def is_valid(self):
         """
-        Method that checks if all connections have been used
+        Check if all connections have been used
         Returns:
-        - True; if all connections have been used
-        - False; if not all connections have been used
+        - True (boolean): if all connections have been used
+        - False (boolean): if not all connections have been used
         """
+
         if sum(self.used.values()) == len(self.used):
             return True
         else:
@@ -161,7 +177,7 @@ class Network():
         
     def calculate_score(self):
         """
-        Method that calculates the score of the network of trajectories 
+        Calculates the score of the network of trajectories 
         """
         
         # all connections that are used in a list
@@ -171,12 +187,15 @@ class Network():
         fraction = (len(used_connections)) / len(self.connections)
         total_time = sum([trajectory.time for trajectory in self.trajectories])
         self.quality_network = fraction * 10000 - (len(self.trajectories) * 100 + total_time) 
-        return fraction*10000, (len(self.trajectories) * 100 + total_time)
     
     def save(self, output_file):
         """
-        Method that saves the network to a csv file 
+        Save the network to a csv file 
+
+        Args:
+        - output_file (string): filepath to save the csv-file to
         """
+
         # number every trajectory for output
         for index, trajectory in enumerate(self.trajectories):
             trajectory.name = index + 1
@@ -189,11 +208,13 @@ class Network():
         #create csv-file from output
         output_df.to_csv(output_file, index=False)
     
-
     def get_score(self):
         """
-        Method that returns the score of the network
+        Returns the score of the network
+        Returns:
+        - quality_network (float): the score of the network that is created
         """
+
         self.calculate_score()
         return self.quality_network
 
